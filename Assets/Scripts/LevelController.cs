5bde3460fd665dd425bcfa3aa4a97bf86eff2273
232171773@qq.com <667976dsa>
2019-07-15 23:19:52 +0800

    finish Main/LevelScene

---
+++ Assets/Scripts/LevelController.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LevelController : MonoBehaviour
+{
+    public void PlayGame() {
+        SceneManager.LoadScene("Gameplay");
+    }
+
+    public void BackToMenu() {
+        SceneManager.LoadScene("MainMenu");
+    }
+}
