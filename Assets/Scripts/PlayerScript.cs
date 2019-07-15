f67b110715fa4d3c9429a0a7cacfce13f7e8a6ee
232171773@qq.com <667976dsa>
2019-07-15 17:51:18 +0800

    create player

---
+++ Assets/Scripts/PlayerScript.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerScript : MonoBehaviour {
+    public float moveForce = 20f, jumpForce = 700f, maxVelocity = 4f;
+    Rigidbody2D myBody;
+    Animator anim;
+    bool grounded = true;
+
+    void Awake() {
+        myBody = GetComponent<Rigidbody2D>();
+        anim = GetComponent<Animator>();
+    }
+
+    void Update() {
+    }
+
+    void FixedUpdate() {
+        PlayerWalkKeyboard();
+    }
+
+    void PlayerWalkKeyboard() {
+        float forceX = 0f;
+        float forceY = 0f;
+        float vel = myBody.velocity.x;
+        float h = Input.GetAxisRaw("Horizontal");
+        if (h > 0) {
+            if (vel < maxVelocity) {
+                forceX = moveForce * (grounded ? 1f : 1.1f);
+            }
+
+            Vector3 scale = transform.localScale;
+            scale.x = 1;
+            transform.localScale = scale;
+            anim.SetBool("Walk", true);
+        }
+        else if (h < 0) {
+            if (vel > -maxVelocity) {
+                forceX = -moveForce * (grounded ? 1f : 1.1f);
+            }
+
+            Vector3 scale = transform.localScale;
+            scale.x = -1;
+            transform.localScale = scale;
+            anim.SetBool("Walk", true);
+        }
+        else {
+            anim.SetBool("Walk", false);
+        }
+
+        if (Input.GetKey(KeyCode.Space)) {
+            if (grounded) {
+                grounded = false;
+                forceY = jumpForce;
+            }
+        }
+
+        myBody.AddForce(new Vector2(forceX, forceY));
+    }
+
+    void OnCollisionEnter2D(Collision2D target) {
+        if (target.gameObject.tag == "Ground") {
+            grounded = true;
+        }
+    }
+}
---
+++ learn.md
@@ -0,0 +1,6 @@
+* 利用变量控制 Animator （anim.SetBool("Walk",true);）
+* 配合 FixedUpdate 实现按键控制
+* 利用刚体位移实现移动
+* transform.localScale 改变人物朝向
+* 无摩擦力的作用下，应考虑合力问题（如空中操作）
+* 为对象的 Collider 的 Material 设置 New Physics Material 2D 来实现弹力 
